b3e7e574e823a90c8d9c824dfa637cf5d6da4b84
tomb2412 <tombramwell610@gmail.com>
2025-02-26 20:38:10 +0000

    restored stopwatch functionality in next app

---
+++ kelder_dash/kelder_dash/app/page.js
@@ -0,0 +1,43 @@
+'use client'
+import {useEffect, useState} from 'react';
+
+export default function Home() {
+
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+  useEffect(() => {
+    let interval;
+    if (running){
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      },10);
+    } else if (!running){
+        clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
+  return (
+    <div className='max-w-md flex flex-col items-center justify-center py-8'>
+      <h1 className='text-2xl font-semibold pb-2 '>Hello Sailor</h1>
+      <p>Lets get on the water</p>
+      <h2>Time under way</h2>
+    <div className='text-xl font-semibold py-4'>
+      <span>{("0" + Math.floor((time / 60000) % 60 )).slice(-2)}:</span>
+      <span>{("0" + Math.floor((time /1000) % 60)).slice(-2)}:</span>
+      <span>{("0" + Math.floor((time /100) % 100)).slice(-2)}</span>
+    </div>
+    <div className='w-1/3 max-w-sm flex flex-row justify-evenly'>
+      {running ? (
+        <button className='border rounded-lg py-1 px-2.5'
+          onClick = {()=> { setRunning(false)} }>Stop</button>
+      ):(
+        <button className='border rounded-lg py-1 px-2.5' 
+        onClick = {()=> { setRunning(true)} }>Start</button>
+      )}
+      <button className='border rounded-lg py-1 px-2.5'
+      onClick = {()=> { setTime(0)}}>Reset</button>
+    </div>
+    </div>
+  );
+}
--- kelder_dash/kelder_dash/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Hello Sailor</h1>
-      <p>Lets get on the water</p>
-    </div>
-  );
-}
