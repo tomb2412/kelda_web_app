ea4789f8d09aea5c3e5665759e267c6317c35218
tomb2412 <tombramwell610@gmail.com>
2025-02-27 21:39:20 +0000

    changed lint ignore to current lint

--- kelder_dash/kelder_dash/app/componants/board.js
+++ kelder_dash/kelder_dash/app/componants/board.js
@@ -5,8 +5,7 @@ const Board = function({task,index,taskList, setTaskList}){
      const  handleDelete = () => {
         let removeIndex = task.indexOf(task);
         taskList.splice(removeIndex, 1);
-        // eslint-disable-next-line no-use-before-define
-        setTaskList((currentTasks => currentTasks.filter(todo => index === 
+        setTaskList((currentTasks => currentTasks.filter(todo => index === // eslint-disable-line no-use-before-define
             removeIndex)));
      }
 
