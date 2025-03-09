5b5004e37cf0446ceea425633e31f679b2da75e5
tomb2412 <tombramwell610@gmail.com>
2025-03-09 07:46:05 +0000

    removed conditional filter around highcharts options

--- kelder_dash/kelder_dash/app/componants/gauge.tsx
+++ kelder_dash/kelder_dash/app/componants/gauge.tsx
@@ -2,7 +2,6 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import HighchartsMore from "highcharts/highcharts-more";
 import HighchartsSolidGauge  from "highcharts/modules/solid-gauge";
-import { type } from "node:os";
 
 if (typeof Highcharts === 'function') {
     HighchartsMore(Highcharts);
@@ -12,62 +11,61 @@ if (typeof Highcharts === 'function') {
 
 
 const Guage = function({}){
-
-    if (typeof Highcharts === 'function') {
-        const chart_options:Highcharts.Options={
-            chart: {
-                type: 'solidgauge'
-            },
-        
-            title: {text:"Fuel"},
-        
-            pane: {
-                startAngle: -90,
-                endAngle: 90,
-                background: [{
-                    backgroundColor:'#fafafa',
-                    borderRadius: 5,
-                    innerRadius: '60%',
-                    outerRadius: '100%',
-                    shape: 'arc'
-                }]
-            },
-
-            // the value axis
-            yAxis: {
-                stops: [
-                    [0.1, '#DF5353'], // red
-                    [0.5, '#DDDF0D'], // yellow
-                    [0.9, '#55BF3B'] // green
-                ],
-                lineWidth: 0,
-                tickWidth: 0,
-                tickAmount: 2,
-                labels: {
-                    y: 16
-                },
-                min: 0,
-                max: 100,
-                title: {
-                    text: '%',
-                    y: -100
-                }
-            },
-
-            series: [{
-                type: "solidgauge",
-                data: [100],
-                tooltip: {
-                    valueSuffix: '%'
-                }
+    
+    const chart_options:Highcharts.Options={
+        chart: {
+            type: 'solidgauge'
+        },
+    
+        title: {text:"Fuel"},
+    
+        pane: {
+            startAngle: -90,
+            endAngle: 90,
+            background: [{
+                backgroundColor:'#fafafa',
+                borderRadius: 5,
+                innerRadius: '60%',
+                outerRadius: '100%',
+                shape: 'arc'
             }]
-        };
-    }
-        return (
-            <div className="border rounded-xl p-3">
-            <HighchartsReact highcharts = {Highcharts} options = {chart_options}/> 
-            </div>
-        )
+        },
+
+        // the value axis
+        yAxis: {
+            stops: [
+                [0.1, '#DF5353'], // red
+                [0.5, '#DDDF0D'], // yellow
+                [0.9, '#55BF3B'] // green
+            ],
+            lineWidth: 0,
+            tickWidth: 0,
+            tickAmount: 2,
+            labels: {
+                y: 16
+            },
+            min: 0,
+            max: 100,
+            title: {
+                text: '%',
+                y: -100
+            }
+        },
+
+        series: [{
+            type: "solidgauge",
+            data: [100],
+            tooltip: {
+                valueSuffix: '%'
+            }
+        }]
+    };
+
+    return (
+        <div className="border rounded-xl p-3">
+        <HighchartsReact highcharts = {Highcharts} options = {chart_options}/> 
+        </div>
+    )
 }
 
 export default Guage
