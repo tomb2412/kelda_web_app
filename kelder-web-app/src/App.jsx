f99c79ece77bbfc266ea3bbed925d1836cf065d0
tomb2412 <tombramwell610@gmail.com>
2025-06-06 22:25:45 +0100

    swapped tile order

--- kelder-web-app/src/App.jsx
+++ kelder-web-app/src/App.jsx
@@ -23,11 +23,11 @@ function App() {
         </div>   */}
         <div className='flex flex-col sm:grid md:grid-cols-2 lg:grid-cols-3 gap-3'>
           <GpsDisplay />
+          <WindRose />
           <DepthGuage />
           {/* <Log /> */}
           <Guage />
           <WindBarb />
-          <WindRose />
           <BilgeDepth />
         </div>
   
