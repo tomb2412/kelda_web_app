dd6d70d1ef4475682fa4047bc36e77330903e10a
Tom Bramwell <tombramwell610@gmail.com>
2025-09-23 08:35:09 +0100

    Added height of tide to depth cards

--- kelder-web-app/src/componants/depth.jsx
+++ kelder-web-app/src/componants/depth.jsx
@@ -1,4 +1,36 @@
+import React, {useState, useEffect, useRef} from 'react';
+
 const DepthGuage = function(){
+
+    const [tidal_event, setTidalEvent] = useState({
+        datetime_stamp: "13:22",
+        height_of_tide: "5.2",
+        event: "HW"
+    })
+
+
+    useEffect(()=> {
+        const requestTidalEvent = async () => {
+            try {
+                const response = await axios.get("http://192.168.1.167:8000/get_next_tidal_event")//"http://raspberrypi.local:8000/gps_coords");//
+                console.log(response.data);
+                setTidalEvent(response.data);
+
+                setError(null);
+            } catch (err) {
+                console.log("Error fetching next tidal event: ", err);
+                setError(null);
+            }
+        };
+
+        requestTidalEvent(); // On startup
+
+        const interval = setInterval(requestTidalEvent, 2000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    
 
     let depth = 17.3
     let unit = "m"
@@ -6,8 +38,9 @@ const DepthGuage = function(){
     return(
         <div className="">
             <div className="grid grid-row-6 rounded-xl p-3 bg-[#024887]/10 text-slate-800 dark:bg-teal-900 dark:text-white mb-3">
-                <span className="text-left text-3xl font-semibold">Depth below Keel</span>
-                <h1 className="flex row-span-4 items-center justify-center text-9xl font-sans font-bold">{depth}</h1>
+                <span className="text-left text-3xl font-semibold">{tidal_event.event === "HW" ? 'High water' : 'Low water'} at {tidal_event.datetime_stamp} with {tidal_event.height_of_tide} m</span>
+                <span className="text-left text-3xl mt-2 font-semibold">Current height of tide:</span>
+                <h1 className="flex row-span-4 items-center justify-center text-9xl font-sans font-bold">3.4</h1>
                 <p className="text-right text-7xl p-3">{unit}</p>
             </div>
             <div>
